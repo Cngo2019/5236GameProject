45bcadf469e1e1ad881b17f9684ed650f255702b
Cngo2019 <canngo215@gmail.com>
2022-09-27 14:43:37 -0400

    level controller created

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -17,24 +17,14 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (spawnTimer >= 0) {
-        //     spawnTimer -= Time.deltaTime;
-        // } else {
-        //     // spawn enemy outside of camera view
-        //     spawnEnemyZombie();
-        //     // reset spawn timer
-        //     spawnTimer = spawnIntervalSec;
-        // }
-
-        float x = 0f;
-        float y = 0f;
-        Vector3 origin = new Vector3(x, y, 0);
-
-        
-        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.zero);
-        if (hit) {
-            Debug.Log("Hit!");
-        } 
+        if (spawnTimer >= 0) {
+             spawnTimer -= Time.deltaTime;
+         } else {
+            //spawn enemy outside of camera view
+            spawnEnemyZombie();
+           // reset spawn timer
+            spawnTimer = spawnIntervalSec;
+         }
 
     }
 
