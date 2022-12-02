dea6fb8a24d346169fd9dea3f36b9beb61efd607
Cngo2019 <canngo215@gmail.com>
2022-12-02 15:33:08 -0500

    fixes

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -12,9 +12,11 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float playerDamage;
     [SerializeField] private WorldDecomposer wd;
 
-    [SerializeField] private float pathFindingTimer;
+    [SerializeField] private float pathFindingTime;
 
     [SerializeField] private float t;
+
+    private int pathIndex;
 
     private GameObject character;
     private float standStillTimer;
@@ -41,12 +43,7 @@ public class EnemyMovement : MonoBehaviour
         
         if (standStillTimer <= 0) {
             if (character != null) {
-                try {
-                    handleMovement();
-                } catch(Exception e) {
-                    Debug.Log(e);
-                }
-                
+                handleMovement();
             }
         }
         else {
@@ -54,47 +51,50 @@ public class EnemyMovement : MonoBehaviour
         }
 
 
-         if (hp <= 0) {
-            levelController.GetComponent<LevelController>().reduceKillCount();
-            GameObject.Destroy(gameObject);
-        }
+        //  if (hp <= 0) {
+        //     levelController.GetComponent<LevelController>().reduceKillCount();
+        //     GameObject.Destroy(gameObject);
+        // }
         
-        if (levelController.GetComponent<LevelController>().getKillRequirement() <= 0) {
-            GameObject.Destroy(gameObject);
-        }
+        // if (levelController.GetComponent<LevelController>().getKillRequirement() <= 0) {
+        //     GameObject.Destroy(gameObject);
+        // }
         
     }
 
     private void handleMovement() {
-        if (computeTimer <= 0) {
-            computePath(character);
-            computeTimer = pathFindingTimer * Time.deltaTime;
-        } else {
+        if (computeTimer > 0) {
             chaseCharacter();
             computeTimer -= Time.deltaTime;
-        }
-        
-    }
-    private void chaseCharacter() {
-         if (path.Count > 0) {
-                Vector2 current = new Vector2(transform.position.x, transform.position.y);
-                if (current.Equals(currentNodeLocation)) {
-                    // Just continue lerping to our current target location.
-                    path.RemoveAt(0);
-                    currentNodeLocation = new Vector2(path[0].getWorldX(), path[0].getWorldZ());
-                    //transform.position = currentNodeLocation;
-                }
-                transform.position = Vector2.MoveTowards(transform.position, currentNodeLocation, t * Time.deltaTime);
-                
         } else {
             computePath(character);
+        }
+    }
+    private void chaseCharacter() {
+         if (pathIndex < path.Count) {
+                Vector2 current = new Vector2(transform.position.x, transform.position.y);
+                if (Vector2.Distance(current, currentNodeLocation) <= .1f) {
+                    pathIndex++;
+                    if (pathIndex < path.Count)
+                        currentNodeLocation = new Vector2(path[pathIndex].getWorldX(), path[pathIndex].getWorldZ());
+                }
+                transform.position = Vector2.MoveTowards(transform.position, currentNodeLocation, t * Time.deltaTime);
+        } else {
+            computePath(character);
+            if (path.Count > 0) {
+                pathIndex = 0;
+                currentNodeLocation = new Vector2(path[pathIndex].getWorldX(), path[pathIndex].getWorldZ());
+            }
         }
     }
 
 
     private void computePath(GameObject player) {
-        int startRow = (int) (transform.position.y + 6 - .5f);
-        int startCol = (int) (transform.position.x + 11 - .5f);
+
+
+        int startRow =  (int) (getNearestWorldNode(transform.position.y) + 6 - .5f);
+        int startCol =  (int) (getNearestWorldNode(transform.position.x) + 11 - .5f);
+
 
         int playerLocationRow = (int) (player.transform.position.y + 6 -.5f);
         int playerLocationCol = (int) (player.transform.position.x + 11 - .5f);
@@ -107,10 +107,20 @@ public class EnemyMovement : MonoBehaviour
             playerLocationCol
         );
 
-        if (path.Count > 0) {
-            currentNodeLocation = new Vector2(path[0].getWorldX(), path[0].getWorldZ());
-        }
+        computeTimer = pathFindingTime * Time.deltaTime;
+
     }
+
+    private float getNearestWorldNode(float y) {
+        float y1 = (int) y;
+        float y_top_nearest = Mathf.Abs((y1 - .5f) - y);
+        float y_bottom_nearest = Mathf.Abs((y1 + .5f) - y);
+
+        if (y_top_nearest < y_bottom_nearest)
+            return y1 - .5f;
+        return y1 + .5f;
+    }
+
 
     void OnTriggerEnter2D(Collider2D collision) {
 
